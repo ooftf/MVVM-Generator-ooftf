0eb57b91422806da06d05aaa04c025beb51d08a1
GitHub Action <action@github.com>
2021-01-11 03:12:18 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/ooftf/androidmvvmgenerator/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.ooftf.androidmvvmgenerator.services
+
+import com.github.ooftf.androidmvvmgenerator.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
